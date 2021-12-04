79fb006fa15cc2cff3eb439c28acbb5a50fbcc48
satyansh sagar <satyansh.sagar@outlook.com>
2021-12-04 23:00:08 +0530

    Added toggle mode

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Header from './bootstrap/Header';
 
 class App extends Component {
   state = {
     Dark: true
   };
+
+  toggleHeader = () => {
+    this.setState({
+      Dark: !this.state.Dark
+    })
+  };
+
   render() {
     return (
       <div className="App">
         {/* <h1>Hello Satyansh</h1> */}
-        <Header dark={this.state.Dark}>Satyansh Portfolio Genrator</Header>
+        <Header dark={this.state.Dark} className="mb-5">Satyansh Portfolio Genrator</Header>
         <div className="container">
           <div className="row">
             <div className="col-12 text-center">
-              <button></button>
+              <button className="btn btn-primary" onClick={this.toggleHeader}>Toggle Mode</button>
             </div>
           </div>
         </div>
