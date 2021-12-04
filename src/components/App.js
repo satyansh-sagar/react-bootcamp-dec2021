cb5b527d259bae50bbbe913124962a4c83afd826
satyansh sagar <satyansh.sagar@outlook.com>
2021-12-04 22:50:58 +0530

    Added a header and state

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
+import Header from './bootstrap/Header';
 
 class App extends Component {
+  state = {
+    Dark: true
+  };
   render() {
     return (
       <div className="App">
-        <h1>Hello Satyansh</h1>
+        {/* <h1>Hello Satyansh</h1> */}
+        <Header dark={this.state.Dark}>Satyansh Portfolio Genrator</Header>
+        <div className="container">
+          <div className="row">
+            <div className="col-12 text-center">
+              <button></button>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
